2dae3166b23df3acd875e02a326642eaad3888a6
zaulsr <zaulsr14@gmail.com>
2021-05-08 11:07:06 +0700

    finish project

--- src/handlers.js
+++ src/handlers.js
@@ -1,8 +1,8 @@
-const nanoId = require('nanoid');
+const { nanoid } = require('nanoid');
 const books = require('./books');
 
 exports.saveBook = (request, h) => {
-  const { pageCount, readPage, name } = request.payload;
+  const { pageCount, readPage, name, reading } = request.payload;
 
   if (!name) {
     const response = h.response({
@@ -22,15 +22,15 @@ exports.saveBook = (request, h) => {
     return response;
   }
 
-  const finished = pageCount === readPage;
+  const id = nanoid(16);
   const insertedAt = new Date().toISOString();
   const updatedAt = insertedAt;
 
   const newBook = {
-    id: nanoId(16),
+    id,
     ...request.payload,
-    reading: false,
-    finished,
+    reading: reading || false,
+    finished: pageCount === readPage,
     insertedAt,
     updatedAt,
   };
@@ -42,6 +42,7 @@ exports.saveBook = (request, h) => {
       status: 'success',
       message: 'Buku berhasil ditambahkan',
       data: {
+        bookId: newBook.id,
         ...newBook,
       },
     });
@@ -57,18 +58,47 @@ exports.saveBook = (request, h) => {
   return response;
 };
 
-exports.getAllBooks = () => {
+exports.getAllBooks = (request) => {
+  let results = [...books];
+  const { name, reading, finished } = request.query;
+
+  if (name) {
+    results = results.filter((book) => book.name.toLowerCase().includes(name.toLowerCase()));
+  }
+
+  if (reading === '1') {
+    results = results.filter((book) => book.reading === true);
+  }
+
+  if (reading === '0') {
+    results = results.filter((book) => book.reading === false);
+  }
+
+  if (finished === '1') {
+    results = results.filter((book) => book.finished === true);
+  }
+
+  if (finished === '0') {
+    results = results.filter((book) => book.finished === false);
+  }
+
+  results = results.map((book) => ({
+    id: book.id,
+    name: book.name,
+    publisher: book.publisher,
+  }));
+
   return {
     status: 'success',
     data: {
-      books,
+      books: results,
     },
   };
 };
 
 exports.getBookById = (request, h) => {
-  const { id } = request.params;
-  const book = books.filter((n) => n.id === id).shift();
+  const { bookId } = request.params;
+  const book = books.filter((n) => n.id === bookId).shift();
 
   if (book) {
     return {
@@ -87,4 +117,74 @@ exports.getBookById = (request, h) => {
   return response;
 };
 
-exports.updateBook = (request, h) => {};
+exports.updateBook = (request, h) => {
+  const { bookId } = request.params;
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload;
+
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  const index = books.findIndex((val) => val.id === bookId);
+  if (index === -1) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+  }
+
+  const updatedAt = new Date().toISOString();
+  books[index] = {
+    ...books[index],
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+    updatedAt,
+  };
+
+  return {
+    status: 'success',
+    message: 'Buku berhasil diperbarui',
+  };
+};
+
+exports.deleteBook = (request, h) => {
+  const { bookId } = request.params;
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index === -1) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Buku gagal dihapus. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+  }
+
+  books.splice(index, 1);
+  return {
+    status: 'success',
+    message: 'Buku berhasil dihapus',
+  };
+};
